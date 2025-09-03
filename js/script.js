0dd867a20ed2b2a9821a9585bf730e903b854970
Catarina Sousa <catarinaasou@gmail.com>
2025-09-03 02:26:27 +0100

    add js footer

---
+++ js/script.js
@@ -0,0 +1,29 @@
+const carousel = document.querySelector('.reviews__carousel');
+const leftArrow = document.getElementById('leftArrow');
+const rightArrow = document.getElementById('rightArrow');
+
+const date = document.getElementById('date');
+
+function getCardWidth() {
+	const card = carousel.querySelector('.reviews__card');
+	return card ? card.offsetWidth + parseInt(getComputedStyle(carousel).gap || 0) : 0;
+}
+
+leftArrow.addEventListener('click', () => {
+	const scrollAmount = getCardWidth();
+	carousel.scrollBy({
+		left: -scrollAmount,
+		behavior: 'smooth',
+	});
+});
+
+rightArrow.addEventListener('click', () => {
+	const scrollAmount = getCardWidth();
+	carousel.scrollBy({
+		left: scrollAmount,
+		behavior: 'smooth',
+	});
+});
+
+const currentDate = () => new Date().getFullYear();
+date.textContent = currentDate();
